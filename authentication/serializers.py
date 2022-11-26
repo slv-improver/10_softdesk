dc2a1b17fdd085e0cf12cbb4b618fe99e5903261
slv-improver <k.moret.slv@gmail.com>
2022-11-26 18:54:00 +0100

    Use validate_password() instead of overwrite create() method

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,9 +13,12 @@ class UserSerializer(serializers.ModelSerializer):
             'password',
         )
 
-    def create(self, validated_data):
-        user = super(UserSerializer, self).create(validated_data)
-        if 'password' in validated_data:
-              user.set_password(validated_data['password'])
-              user.save()
-        return user
+    def validate_password(self, value):
+        return make_password(value)
+
+    # def create(self, validated_data):
+    #     user = super(UserSerializer, self).create(validated_data)
+    #     if 'password' in validated_data:
+    #           user.set_password(validated_data['password'])
+    #           user.save()
+    #     return user
