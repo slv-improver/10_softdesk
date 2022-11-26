4dc8663100e15a139acc72d535056b4b1cc1d413
slv-improver <k.moret.slv@gmail.com>
2022-11-26 17:55:50 +0100

    Move token paths to authentication.urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
 
 from . import views
 
@@ -12,4 +13,14 @@ urlpatterns = [
     # Include login/ & logout/ paths
     path('', include('rest_framework.urls')),
     path('', include(router.urls)),
+    path(
+        'api/token/',
+        jwt_views.TokenObtainPairView.as_view(),
+        name='token_obtain_pair',
+    ),
+    path(
+        'api/token/refresh/',
+        jwt_views.TokenRefreshView.as_view(),
+        name='token_refresh',
+    ),
 ]
