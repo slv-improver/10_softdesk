3ba7bcb1cd101114b92d1457cdee8065e661d62c
slv-improver <k.moret.slv@gmail.com>
2022-12-27 14:25:07 +0100

    Get contributors with Project details

--- project/serializers.py
+++ project/serializers.py
@@ -3,6 +3,13 @@ from rest_framework import serializers
 from . import models
 
 
+class ContributorListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Contributor
+        fields = ('id', 'user', 'project')
+
+
 class ProjectListSerializer(serializers.ModelSerializer):
     
     class Meta:
@@ -12,6 +19,13 @@ class ProjectListSerializer(serializers.ModelSerializer):
 
 class ProjectDetailSerializer(serializers.ModelSerializer):
 
+    contributors = serializers.SerializerMethodField()
+
     class Meta:
         model = models.Project
-        fields = ('id', 'title', 'description', 'type')
+        fields = ('id', 'title', 'description', 'type', 'contributors')
+
+    def get_contributors(self, instance):
+        queryset = models.Contributor.objects.filter(project_id=instance.id)
+        serializer = ContributorListSerializer(queryset, many=True)
+        return serializer.data
